e0de1b7c86a9a3ff908b5f153b07193b07a291d3
MilaNedic <49908385+MilaNedic@users.noreply.github.com>
2019-11-17 16:26:38 +0100

    Update kolokvij-2017-12-20.ml

--- izpiti/2017-12-20/kolokvij-2017-12-20.ml
+++ izpiti/2017-12-20/kolokvij-2017-12-20.ml
@@ -4,28 +4,34 @@
 
 (* 1.1) Definirajte funkcijo, ki vzame par in zamenja komponenti para.
    Primer: /obrni (2, 4) = (4, 2)/ *)
- let obrni = failwith "dopolni me"
+ let obrni (x, y) = (y, x)
 
 (* 1.2) Definirajte funkcijo, ki vzame par p in vrednost x in zamenja drugo
    komponento para p z x.
    Primer: /zamenjaj_drugo (2, 4) 7 = (2, 7)/ *)
- let zamenjaj_drugo = failwith "dopolni me"
+ let zamenjaj_drugo (a, b) x = (a, x)
 
 (* 1.3) Definirajte funkcijo, ki vzame seznam parov in izračuna nov seznam parov,
    ki imajo drugo komponento zamenjano z 42.
    Primer: /vsem_zamenjaj_drugo_z_42 [(12, 1); (2, 4)] = [(12, 42); (2, 42)]/ *)
- let vsem_zamenjaj_drugo_z_42 = failwith "dopolni me"
+ let rec vsem_zamenjaj_drugo_z_42 list = 
+  match list with
+  | [] -> []
+  | (a, b) :: xs -> (a, 42) :: vsem_zamenjaj_drugo_z_42 xs
 
 (* 1.4) Definirajte funkcijo, ki varno vrne glavo seznama v primeru, ko seznam ni prazen.
    Uporabite tip option.
    Primer: /glava [1; 2; 3] = Some 1/ *)
- let glava = failwith "dopolni me"
+ let glava = function
+  | [] -> None
+  | x :: xs -> Some x
 
 (* 1.5) Definirajte funkcijo, vzame funkcijo (f: 'a -> 'b), neko vrednost (x : 'a) in
    celo število n. Funkcija naj vrne vrednost, ki jo dobimo če f n-krat uporabimo na x,
    torej f (f ... (f x)...).
    Primer: /uporabi_veckrat succ 0 420 = 420/ *)
- let uporabi_veckrat = failwith "dopolni me"
+ let rec uporabi_veckrat f n x = function
+  | x -> if n <= 0 then x else f (n - 1) x
 
 (* ======================================= *)
 (* 2. naloga: podatkovni tipi in rekurzija *)
@@ -36,8 +42,15 @@
    tipom /'a drevo/ z enim konstruktorjem, ki sprejme:
    - vrednost (koren) tipa /'a/ in
    - seznam (gozd) dreves tipa /'a drevo/. *)
-type 'a drevo = DopolniMe
 
+(* 
+splošno drevo
+type 'a drevo =
+  | Prazno
+  | Sestavljeno of 'a drevo * 'a * 'a drevo 
+*)
+
+type 'a drevo = Drevo of ('a * ('a drevo list))
 (* 2.2) Definirajte naslednja rožna drevesa:
 
    t = 1,  t' =  2   ,      t'' =  3
@@ -47,9 +60,9 @@ type 'a drevo = DopolniMe
 
  *)
 
-let t = failwith "dopolni me"
-let t' = failwith "dopolni me"
-let t'' = failwith "dopolni me"
+let t = Drevo(1, [])
+let t' = Drevo(2, [t; t])
+let t'' = Drevo(3, [Drevo(-1, []); t'; Drevo(0, [])])
 
 (* 2.3) Definirajte funkcijo, ki vrne gozd rožnega drevesa. *)
 let vrni_gozd = failwith "dopolni me"
@@ -79,3 +92,4 @@ let globoko_drevo = failwith "dopolni me"
    na primer List.map, niso repno rekurzivne, zato se jim raje
    izognite. *)
 let zlozi = failwith "dopolni me"
+
